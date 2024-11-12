4bc3c15adfbbc521b0e7169414afe86afe9db581
Damon Gonzalez <damon.r.gonzalez@gmail.com>
2024-11-12 07:53:32 -0800

    Initial Commit

---
+++ 3DImmersionVisionPro/_DImmersionVisionProApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DImmersionVisionProApp.swift
+//  3DImmersionVisionPro
+//
+//  Created by Damon gonzalez on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct _DImmersionVisionProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 3DImmersionVisionProTests/_DImmersionVisionProTests.swift
@@ -0,0 +1,17 @@
+//
+//  _DImmersionVisionProTests.swift
+//  3DImmersionVisionProTests
+//
+//  Created by Damon gonzalez on 11/12/24.
+//
+
+import Testing
+@testable import _DImmersionVisionPro
+
+struct _DImmersionVisionProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
